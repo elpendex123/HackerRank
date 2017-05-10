3d25905aa5a6a5445035c6d95653f77ebb7479db
Enrique Coello <enrique.coello@gmail.com>
2017-05-09 20:40:57 -0400

    Finished Find The Bug

--- src/main/java/com/coello/enrique/FindTheBug.java
+++ src/main/java/com/coello/enrique/FindTheBug.java
@@ -5,35 +5,40 @@ import java.util.Scanner;
 public class FindTheBug {
 
     static int[] findTheBug(String[] grid){
-        // Complete this function
     	int [] array = new int[2];
-    	array[0] = 0;
-    	array[1] = 0;
+    	int row = 0;
+    	int column = 0; 
+    	
+    	for (row = 0; row < grid.length; row++)
+    	{
+    		if (grid[row].contains("X"))
+    		{
+    			column = grid[row].lastIndexOf("X");
+    			break;
+    		}
+    	}
+
+    	array[0] = row;
+    	array[1] = column;
+    	
     	return array;
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        n=3;
         String[] grid = new String[n];
         
-//        for(int grid_i=0; grid_i < n; grid_i++){
-//            grid[grid_i] = in.next();
-//        }
-        
-        grid[0] = "OOO";
-        grid[1] = "O1O";
-        grid[2] = "OOO";
-        
-        // Return an array containing [r, c]
+        for(int grid_i=0; grid_i < n; grid_i++){
+            grid[grid_i] = in.next();
+        }
+
         int[] result = findTheBug(grid);
         
         for (int i = 0; i < result.length; i++) {
             System.out.print(result[i] + (i != result.length - 1 ? "," : ""));
         }
         
-        System.out.println("");
         in.close();
     }
 }
